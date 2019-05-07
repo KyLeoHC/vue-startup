6e29d8dcfa2801e2c2308865aa8d37ba1ecc5c7f
KyLeoHC <kyleo.hc@foxmail.com>
2019-05-07 09:31:00 +0800

    chore(babel.config.js): 格式调整

--- babel.config.js
+++ babel.config.js
@@ -1,66 +1,64 @@
 module.exports = api => {
   const isTest = api.env('test');
-  const presetEnvConfig = isTest ?
-    {
-      targets: { node: 'current' }
-    } :
-    {
-      // "debug": true,
-      modules: false,
-      useBuiltIns: "usage",
-      corejs: {
-        "version": "3"
-      }
-    };
+  const presetEnvConfig = isTest ? {
+    targets: { node: 'current' }
+  } : {
+    // debug: true,
+    modules: false,
+    useBuiltIns: 'usage',
+    corejs: {
+      'version': '3'
+    }
+  };
   return {
     presets: [
       [
-        "@babel/preset-env",
+        '@babel/preset-env',
         presetEnvConfig
       ],
       [
-        "@babel/preset-typescript",
+        '@babel/preset-typescript',
         {
           // 目前vue-loader解析出来的ts代码传递给babel-loader处理时，没能认出是ts代码
           // 所以这里强制所有代码当成ts或者tsx处理
-          "isTSX": true,
-          "allExtensions": true
+          'isTSX': true,
+          'allExtensions': true
         }
       ]
     ],
     plugins: [
       [
-        "@babel/plugin-proposal-decorators",
+        '@babel/plugin-proposal-decorators',
         {
-          // "decoratorsBeforeExport": true,
-          "legacy": true
+          // 'decoratorsBeforeExport': true,
+          'legacy': true
         }
       ],
       [
-        "@babel/proposal-class-properties",
+        '@babel/proposal-class-properties',
         {
-          "loose": true
+          'loose': true
         }
       ],
-      "@babel/proposal-object-rest-spread",
+      '@babel/proposal-object-rest-spread',
       [
-        "@babel/plugin-syntax-dynamic-import"
+        '@babel/plugin-syntax-dynamic-import'
       ],
       [
-        "@babel/plugin-transform-runtime",
+        '@babel/plugin-transform-runtime',
         {
-          "corejs": false,
-          "helpers": true,
-          "regenerator": true,
-          "useESModules": true
+          'corejs': false,
+          'helpers': true,
+          'regenerator': true,
+          'useESModules': true
         }
       ],
       [
-        "import",
+        'import',
         {
-          "libraryName": "vant",
-          "libraryDirectory": "es",
-          "style": name => `${name}/style/less`
+          'libraryName': 'vant',
+          'libraryDirectory': 'es',
+          'style': name => `${name}/style/less`
         }
       ]
     ]
