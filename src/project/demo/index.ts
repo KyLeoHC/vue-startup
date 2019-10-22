eb2f23ed1cd5ae6314a51758cedf148ac55ea19b
KyLeoHC <kyleo.hc@foxmail.com>
2019-10-22 15:08:49 +0800

    refactor: add 'polyfill.ts' file

--- src/project/demo/index.ts
+++ src/project/demo/index.ts
@@ -3,6 +3,7 @@ import Vue, { VNode } from 'vue';
 import {
   loadCSSByArray
 } from '@/utils';
+import polyfill from '@/common/polyfill';
 import router from './router';
 import App from './app.vue';
 import {
@@ -13,14 +14,12 @@ import {
 // import vConsole from 'vconsole';
 // new vConsole();
 
+polyfill();
+
 Vue.use(List)
   .use(NavBar)
   .use(PullRefresh);
 
-// 这里之所以会加多一段Promise.resolve().finally()冗余代码
-// 是因为babel7在面对finally但是没发现Promise关键字的时候
-// 就算你目标函数内部返回值是Promise，babel也不会识别出并且polyfill这个finally方法;
-Promise.resolve().finally();
 loadCSSByArray([
   '//at.alicdn.com/t/font_1007376_mqnhabrqmch.css',
   ...(window.__cssList || [])
--- src/project/demo2/index.ts
+++ src/project/demo2/index.ts
@@ -3,16 +3,14 @@ import Vue, { VNode } from 'vue';
 import {
   loadCSSByArray
 } from '@/utils';
+import polyfill from '@/common/polyfill';
 import store from './store';
 import router from './router';
 import App from './app.vue';
 // import vConsole from 'vconsole';
 // new vConsole();
 
-// 这里之所以会加多一段Promise.resolve().finally()冗余代码
-// 是因为babel7在面对finally但是没发现Promise关键字的时候
-// 就算你目标函数内部返回值是Promise，babel也不会识别出并且polyfill这个finally方法
-Promise.resolve().finally();
+polyfill();
 loadCSSByArray([
   '//at.alicdn.com/t/font_1007376_mqnhabrqmch.css',
   ...(window.__cssList || [])
