952c055f2eaf3451cf987bc3cff533eb801b093a
KyLeoHC <kyleo.hc@foxmail.com>
2019-11-07 09:24:14 +0800

    fix: add missing file

---
+++ src/project/demo2/views/vuexTest/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="vuex-test-container">
+    <logo/>
+    <div>{{ newContent }}</div>
+    <div>work with vuex</div>
+  </div>
+</template>
+<script src="./index.ts"></script>
+<style lang="scss">
+  @import "~styles/common";
+
+  .vuex-test-container {
+    font-size: 18px;
+    text-align: center;
+  }
+</style>
